63222af0bf7ac1e3e842e40aec5c7090bcc4b5cc
Shrikant sherkar <shreex10008@gmail.com>
2020-06-08 16:16:46 +0530

    CollapsibleToolbar added

--- CollapsibleToolbar/CollapsibleToolbar/MainPage.xaml.cs
+++ CollapsibleToolbar/CollapsibleToolbar/MainPage.xaml.cs
@@ -8,14 +8,41 @@ using Xamarin.Forms;
 
 namespace CollapsibleToolbar
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        double _toolbarPosition;
         public MainPage()
         {
             InitializeComponent();
+            TheScroll.PropertyChanged += OnScrollViewPropertyChanged;
+            collapsibleContent.SizeChanged += CollapsibleContentSizeChanged;
+            toolbarStack.SizeChanged += OnTitleTextSizeChanged;
+            headerLbl.Opacity = 0;
+        }
+
+        private void OnTitleTextSizeChanged(object sender, System.EventArgs e)
+        {
+            toolbarStack.SizeChanged -= OnTitleTextSizeChanged;
+            _toolbarPosition = toolbarStack.Y + 1;
+        }
+
+        private void CollapsibleContentSizeChanged(object sender, System.EventArgs e)
+        {
+            collapsibleContent.SizeChanged -= CollapsibleContentSizeChanged;
+            toolbarStack.Margin = new Thickness(0, collapsibleContent.Height - 50, 0, 0);
+        }
+
+        private void OnScrollViewPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName.Equals(ScrollView.ScrollYProperty.PropertyName))
+            {
+                var scrolled = ((ScrollView)sender).ScrollY;
+                toolbarStack.TranslationY = scrolled < _toolbarPosition ? 0 - scrolled : 0 - _toolbarPosition;
+                var opac = -Math.Round(100 * toolbarStack.TranslationY / 150) / 100;
+                if (opac >= 0 || opac <= 1)
+                    headerLbl.Opacity = opac;
+            }
         }
     }
 }
